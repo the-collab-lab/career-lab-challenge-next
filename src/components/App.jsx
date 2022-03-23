df2475834ec54a7660154c89a35b61a73855a72c
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-03-22 18:21:25 -0700

    chore: add comments to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,12 @@ import { SearchForm } from './SearchForm';
 
 export function App() {
 	function onSearchSubmit(query) {
+		// Search for the users's query, then log the results
+		// to the console.
+		// NOTE: `searchArtworks` currently returns local data, so that we
+		// don't make too many requests to the COIA API! Once we've built out
+		// our UI, we need to make real requests to the API!
+		// @see: ./src/uitls/api.js
 		searchArtworks(query).then((json) => {
 			console.log(json);
 		});
