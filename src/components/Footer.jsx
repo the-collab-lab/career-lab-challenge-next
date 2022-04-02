ab6f04fa793b3809926ad548f64eaacfd50a2d4a
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-04-02 12:47:28 -0700

    chore: fix className attribute

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import './Footer.css';
 
 export const Footer = () => (
-	<footer class="Footer">
+	<footer className="Footer">
 		<p>
 			Artwork and all related information courtesy of{' '}
 			<a href="https://www.artic.edu/">Art Institute of Chicago</a>.
