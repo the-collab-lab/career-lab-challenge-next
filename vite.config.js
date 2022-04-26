ab3f9169b3a4f8c60ddf8e8f124207d641c6eaf3
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-04-26 07:54:17 -0700

    chore: add eslint plugin

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import eslint from '@nabla/vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
@@ -36,7 +37,12 @@ const PWAConfig = {
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: { outDir: './build' },
-	plugins: [VitePWA(PWAConfig), react(), splitVendorChunkPlugin()],
+	plugins: [
+		eslint({ formatter: 'stylish' }),
+		react(),
+		splitVendorChunkPlugin(),
+		VitePWA(PWAConfig),
+	],
 	server: { open: true },
 	test: {
 		globals: true,
